411dcf0e2cf2bc12662978db57400cf756ff7875
minhajul08 <minhazmehadi61@gmail.com>
2024-05-04 11:32:32 +0600

    server connect with client side

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require ('express');
+const cors = require ('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express ();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use (cors ());
+app.use (express.json())
+
+// tour-ease
+// GtNpAXIjn0MLrgTl
+
+
+const uri = "mongodb+srv://tour-ease:GtNpAXIjn0MLrgTl@cluster0.01tlpf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const TouristCollection =client.db ('Tourist').collection ('touristSpot');
+     app.get ('/addTouristSpot', async (req,res) => {
+      const cursor = TouristCollection.find ()
+      const result = await cursor.toArray ();
+      res.send (result)
+     })
+
+    app.post ('/addTouristSpot', async (req,res)=>{
+      const tourist = req.body
+      console.log (tourist)
+      const result = await TouristCollection.insertOne (tourist)
+      res.send (result);
+    })
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get ('/', (req,res)=> {
+    res.send ("assignment 10 server is Running")
+})
+
+app.listen (port, () => {
+    console.log (`Assignment 10 server is running on port, ${port}`)
+})
