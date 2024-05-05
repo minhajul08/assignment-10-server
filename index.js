85e5f10acea805e24f3fbc841ec9bb8017f47c1c
minhajul08 <minhazmehadi61@gmail.com>
2024-05-05 12:40:07 +0600

    update added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const cors = require ('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require ('dotenv').config ();
+const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
 const app = express ();
 const port = process.env.PORT || 5000;
 
@@ -8,11 +9,8 @@ const port = process.env.PORT || 5000;
 app.use (cors ());
 app.use (express.json())
 
-// tour-ease
-// GtNpAXIjn0MLrgTl
 
-
-const uri = "mongodb+srv://tour-ease:GtNpAXIjn0MLrgTl@cluster0.01tlpf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.01tlpf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -49,7 +47,47 @@ async function run() {
        const result = await TouristCollection.find ({email:req.params.email}).toArray ();
        res.send (result)
      })
+     
+     app.get ("/spots/:id", async (req,res) => {
+      console.log (req.params.id)
+      const result = await TouristCollection.findOne ({_id: new ObjectId (req.params.id), 
+      });
+      console.log (result)
+      res.send (result)
+     })
 
+     app.put ("/update/:id", async (req,res) => {
+       const id = req.params.id;
+       const filter = {_id: new ObjectId (id)}
+       const options ={upset:true};
+       const spotUpdated = req.body;
+      const updated = {
+        $set: {
+          spot: spotUpdated .spot,
+          country : spotUpdated.country,
+          season: spotUpdated.season,
+          cost : spotUpdated.cost,
+          location : spotUpdated.location,
+          description : spotUpdated.description,
+          visitor : spotUpdated.visitor,
+          time : spotUpdated.time,
+          name : spotUpdated.name,
+          email : spotUpdated.email,
+           photo: spotUpdated.photo,
+
+        }
+      }
+      const result = await TouristCollection.updateOne (filter, updated, options);
+      res.send (result);
+
+     })
+
+
+     app.delete ('/delete/:id',async (req,res) => {
+      const result = await TouristCollection.deleteOne ({_id: new ObjectId (req.params.id)})
+      console.log (result);
+      res.send (result)
+     })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
